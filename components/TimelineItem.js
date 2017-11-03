8458b959e5136a66dcd6581722d34542c0093907
John Cipponeri <john.cipponeri@gmail.com>
2017-11-03 09:52:10 -0500

    Create view for timeline item

---
+++ components/TimelineItem.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+const TimelineItem = ({
+    id,
+    title,
+    subTitle,
+    color,
+    backgroundColor,
+    icon,
+    judgementIcon,
+    evaluation,
+    notes,
+}) => {
+    return (
+        <div className='timelineItem'>
+            <h1>{title}</h1>
+
+            {!subTitle && 
+                <hr /> 
+            }
+
+            {subTitle && 
+                <h2>{subTitle}</h2>
+            }
+
+            {evaluation &&
+                <div className='evaluation'>
+                    <p className='fl'>{evaluation.text}</p>
+                    {evaluation.score &&
+                        <p className='fr'>{evaluation.score}/{evaluation.maxScore}</p>
+                    }
+                </div>
+            }
+
+            {notes &&
+                <p>{notes}</p>
+            }
+        </div>  
+    );
+}
+
+export default TimelineItem;
