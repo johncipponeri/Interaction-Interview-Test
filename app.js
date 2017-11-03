1a7776b20424e57a9b2fd132821617b7c7609bbd
John Cipponeri <john.cipponeri@gmail.com>
2017-11-03 09:13:58 -0500

    Basic react/es6/webpack config

---
+++ app.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'React';
+import ReactDOM from 'react-dom';
+
+const styles = {
+
+}
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                nice
+            </div>
+        )
+    }
+}
+
+const root = document.querySelector('#app');
+ReactDOM.render(<App />, root);
